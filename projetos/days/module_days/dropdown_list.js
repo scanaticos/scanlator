1b2c70392ae61e6ab09ff18b8cd13e05f51b02b8
scanaticos <scanatio@gmail.com>
2020-10-09 15:43:58 -0300

    add cap 5 de days

--- projetos/days/module_days/dropdown_list.js
+++ projetos/days/module_days/dropdown_list.js
@@ -1,7 +1,7 @@
 function addDropList(){
     const dropMenu = document.getElementById("dropdown-menu")
 
-    for(let c = 1; c <= 4; c++){
+    for(let c = 1; c <= 5; c++){
         const capName  = document.createElement("a")
         capName.className = "dropdown-item"
         capName.innerHTML = `Cap. ${c}`
