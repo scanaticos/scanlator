504cc91e84cfc62c93dd2a93c7765ef408dbd3f3
scanaticos <scanatio@gmail.com>
2020-10-09 07:55:52 -0300

    automatizando atualização do dropdowm do leitor

---
+++ projetos/shangrila_frontier/module_shangrila/dropdown_list.js
@@ -0,0 +1,12 @@
+function addDropList(){
+    const dropMenu = document.getElementById("dropdown-menu")
+
+    for(let c = 1; c <= 3; c++){
+        const capName  = document.createElement("a")
+        capName.className = "dropdown-item"
+        capName.innerHTML = `Cap. ${c}`
+        capName.href = `./${c}.html` 
+        dropMenu.appendChild(capName)
+    }
+}
+addDropList()
