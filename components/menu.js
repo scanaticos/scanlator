7230b07965ae9ee5ce4925d1e8c795b86527e867
scanaticos <scanatio@gmail.com>
2020-10-15 12:01:41 -0300

    concertando menu nas pages home e index + add a aba de parceiros

--- components/menu.js
+++ components/menu.js
@@ -8,26 +8,32 @@ function exibirMenu(){
     //Opt Menu
     const home = document.createElement("a")
     const projetos = document.createElement("a")
+    const parceiros = document.createElement("a")
 
     //Escrevendo o texto
     home.innerHTML = "Home"
     projetos.innerHTML = "Projetos"
+    parceiros.innerHTML = "Parceiros"
 
     //Atribuindo classe
     home.className = classe
     projetos.className = classe
+    parceiros.className = classe
 
     //Atribuindo link
     home.href = "https://scanaticos.site/home.html"
     projetos.href = "https://scanaticos.site/projetos.html"
+    parceiros.href = "./../parceiros.html"
 
     //Estilizando
     home.style = "margin-right: 20px;"
     projetos.style = "margin-right: 20px;"
+    parceiros.style = "margin-right: 20px;"
 
     //Atribuindo ao Pai
     navbarNav.appendChild(home)
     navbarNav.appendChild(projetos)
+    navbarNav.appendChild(parceiros)
 }
 
 exibirMenu()
