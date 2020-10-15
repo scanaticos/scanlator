795d08ab7c7c6852823d140a9c8990ca6da6867c
scanaticos <scanatio@gmail.com>
2020-10-15 19:32:34 -0300

    testando views sem autenticação

--- projetos/days/database/views.js
+++ projetos/days/database/views.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", async function (){
     firebase.database().ref("days").once("value").then(async function(snapshot) {
         const url = window.location.href
         console.log(url.slice(0,45))
-        if(url.slice(0, 45) == "https://scanaticos.site/projetos/days/capitulos"){
+        /*        if(url.slice(0, 45) == "https://scanaticos.site/projetos/days/capitulos"){
             
         if(!!document.getElementById("views")){
             await visitou(snapshot.val())
@@ -21,6 +21,8 @@ document.addEventListener("DOMContentLoaded", async function (){
         }else{
             await visitou(snapshot.val())
         }
-        } 
+        }  */
+        await visitou(snapshot.val())
+        await mostrarVisitantes(snapshot.val())
     })
 })
