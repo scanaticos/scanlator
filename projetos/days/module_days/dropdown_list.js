3e8613fd7ccdef212de57ce24280265ef805cdd6
scanaticos <scanatio@gmail.com>
2020-10-16 14:30:54 -0300

    leior de days atualizado

--- projetos/days/module_days/dropdown_list.js
+++ projetos/days/module_days/dropdown_list.js
@@ -8,6 +8,10 @@ function addDropList(){
         capName.innerHTML = `Cap. ${c}`
         capName.href = `./${c}.html` 
         dropMenu.appendChild(capName)
+
+        if(c == Number(dropdownMenuLink.text.split("")[24]+dropdownMenuLink.text.split("")[25])){
+            capName.className = "dropdown-item active"
+        }
     }
 
     for(let c = 45; c <= 45; c++){
