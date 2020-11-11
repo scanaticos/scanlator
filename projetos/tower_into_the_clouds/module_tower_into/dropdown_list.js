627489fdef59a6d7bcdadea0292bba32ee5b5a72
scanaticos <scanatio@gmail.com>
2020-11-11 20:17:43 -0300

    adicionando cap 24 de tower into the clouds +  concertando o voltar a pag no fundo de todos os caps de tower into the clouds

--- projetos/tower_into_the_clouds/module_tower_into/dropdown_list.js
+++ projetos/tower_into_the_clouds/module_tower_into/dropdown_list.js
@@ -1,7 +1,7 @@
 function addDropList(){
     const dropMenu = document.getElementById("dropdown-menu")
 
-    for(let c = 21; c <= 23; c++){
+    for(let c = 21; c <= 24; c++){
         const capName  = document.createElement("a")
         capName.className = "dropdown-item"
         capName.innerHTML = `Cap. ${c}`
